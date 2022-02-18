6f859b4fa2db56e36b9c3cab5f81fe8da3d13629
Heide van der <r.van.der.heide@accell-it-services.com>
2022-02-18 10:27:51 +0100

    eerste scripts

---
+++ apitest.py
@@ -0,0 +1,30 @@
+from importlib.resources import path
+from operator import gt
+from pathlib import Path
+from pydoc import describe
+from fastapi import FastAPI, Path, Query
+from typing import Optional
+
+app = FastAPI()
+
+inventory = {
+        1: {  
+            "name": "test",
+            "price": 2.56,
+            "brand": "test"     
+
+         }
+    }
+
+@app.get("/get-item/{item_id}")
+def get_item(item_id: int = Path(None, description="kies een id van de lijst")):
+    return inventory[item_id]
+
+@app.get("/get-item_name/")
+def get_item(name: str = Query(None, title="name", description="name of item")):
+    for item_id in inventory:
+        if inventory[item_id]["name"] == name:
+            return inventory[item_id]
+    return {"data": "not found"}
+
+
