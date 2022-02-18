4d7c52b05f5b65fc9a6738f621bac8f4e6212e30
robert vd heide <robertjuhh70@hotmail.com>
2022-02-18 14:58:56 +0100

    export sjon file

--- import requests.py
+++ import requests.py
@@ -1,29 +1,33 @@
-#api module
 import requests
+import json
 
-#informatie api
 API_KEY = "31b982a2c2810668a92fb498be57fc56"
 BASE_URL = "http://api.openweathermap.org/data/2.5/weather"
 
-#stad uit de database kiezen, met een f string de url bepalen waar de request vandaan moet komen
-city = "amsterdam"
+city = input("Enter a city name: ")
 request_url = f"{BASE_URL}?appid={API_KEY}&q={city}"
 response = requests.get(request_url)
 
-#response code 200 is goed dit betekent geslaagd, daarna import hij de data van de stad in json formaat
+
 if response.status_code == 200:
     data = response.json()
-    #uit het json bestand definieer ik wat ik beschreven wil zien
+    
+    name= data["name"]
     weather= data['weather'][0]['description']
     temperature= round(data["main"]["temp"] - 273.15, 2)
     min_degrees= round(data['main']['temp_min'] - 273.15, 2)
-    max_degrees= round(data['main']['temp_max'] - 273.15, 2) 
+    max_degrees= round(data['main']['temp_max'] - 273.15, 2)
 
-    #print de gedifineerde data
+    totaal = ("city", name), ("weather", weather), ("temperature", temperature), ("min_degrees", min_degrees), ("max_degrees", max_degrees)
+    with open("graden.json", "w") as f:
+        json.dump(totaal, f, indent=2)
+    
+    '''print("city:", name)
     print("Weather:", weather)
     print("Temperature:", temperature, "celsius")
     print("min_degrees:", min_degrees, "celsius")
-    print("max_degrees:", max_degrees, "celcius")
-#als de code niet 200 is krijg je deze melding, kan fout met api key of url zijn
+    print("max_degrees:", max_degrees, "celcius")'''
+
 else:
-    print("An error occurred.")
+    print("An error occurred.")   
+
