417290cb84f94ff0a4fdbd7725cfb6f3f59f313c
robert <robertjuhh70@hotmail.com>
2022-02-18 16:15:52 +0100

    push

--- compare.py
+++ compare.py
@@ -8,7 +8,8 @@ first_file_lines = open(first_file).readlines()
 second_file_lines = open(second_file).readlines()
 
 
-difference = difflib.HtmlDiff().make_file(first_file_lines, second_file_lines,  first_file, second_file, )
+
+difference = difflib.HtmlDiff().make_file(first_file_lines, second_file_lines, first_file, second_file,)
 difference_report = open("difference_report.html", "w")
 difference_report.write(difference)
 difference_report.close()
